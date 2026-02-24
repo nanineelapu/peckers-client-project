6f8c77e75f248e909d8daac6eb038406a3eadedd
Neelapu Nani <neelapunani2580@gmail.com>
2026-02-24 23:06:26 +0530

    Finished our Story

--- app/ourstory/ActualCircle2.jsx
+++ app/ourstory/ActualCircle2.jsx
@@ -5,10 +5,10 @@ import React from "react";
 const timelineData = [
     {
         year: "APR 2022 & OCT 2022",
-        title: "Hitchin & StevWALTON & MEPPERSHALL enage",
+        title: "HITCHIN & STEVWALTON & MEPPERSHALL ENAGE",
         description:
-            "Back where it started. Peckers opens in the same spot as Grandad's original store.",
-        borderStyle: "border border-zinc-700",
+            "Two new locations. Small footprint, big impact.",
+        borderStyle: "border-dashed border-zinc-700",
     },
     {
         year: "MARCH 2020",
@@ -29,20 +29,20 @@ const timelineData = [
 export default function PeckersTimeline2() {
 
     return (
-        <div className="bg-black text-white flex justify-center mt-[12vw] py-[5vw] font-peckers">
-            <div className="w-full max-w-[90vw] relative">
+        <div className="bg-black text-white flex justify-center mt-[13vw] py-[4vw] font-peckers">
+            <div className="w-full max-w-[88vw] relative">
 
                 {/* Cards */}
-                <div className="flex justify-between px-[2vw] gap-[1vw] h-[4vw] items-center mb-[8vw]">
+                <div className="flex justify-between gap-[9vw] h-[4vw] items-center mb-[8vw]">
                     {timelineData.map((item, index) => {
 
                         const alignment =
                             index === 0
-                                ? "items-end text-right "
+                                ? "items-end text-right"
                                 : index === 1
                                     ? "items-center text-center"
                                     : "items-start text-left";
-                        const offset = index === 1 ? "-mb-[3vw]" : "";
+                        const offset = index === 1 ? "-mt-[-6vw]" : "";
 
 
                         return (
@@ -54,15 +54,15 @@ export default function PeckersTimeline2() {
                                         : {}
                                 }
                                 className={`relative px-[1vw] py-[1vw] rounded-[1vw] flex flex-col ${alignment} ${offset} w-[17vw] h-[12vw]
-  bg-[#121212] ${item.highlight
-                                        ? "border border-yellow-500/70"
-                                        : `border ${item.borderStyle || "border-zinc-800"}`
+  ${item.highlight
+                                        ? "bg-[#121212] border border-yellow-500/60"
+                                        : `bg-[#0a0a0a] border ${item.borderStyle || "border-zinc-800"}`
                                     }`}
                             >
                                 <span
-                                    className={`text-[1.65vw]  font-bold mb-[1.5vw] tracking-tight ${item.highlight ? "text-white" : "text-zinc-600"
+                                    className={`text-[1.65vw] font-bold mb-[1.5vw] tracking-tight ${item.highlight ? "text-white" : "text-zinc-600"
                                         }`}
-                                    style={{ fontFamily: "Space Mono", color:"#ffff" }}
+                                    style={{ fontFamily: "Space Mono" }}
                                 >
                                     {item.year}
                                 </span>
@@ -72,7 +72,7 @@ export default function PeckersTimeline2() {
                                 </h3>
 
                                 <p className="text-zinc-500 font-peakers text-[0.85vw] leading-none font-medium"
-                                    style={index === 1 ? { fontSize: ".9vw" } : {}}>
+                                    style={index === 1 ? { fontFamily: "Space Mono", fontSize: ".7vw" } : {}}>
 
                                     {item.description}
                                 </p>
