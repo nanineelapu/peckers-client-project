9a10f4f49568b6a82867a5135ac98a1ee8da24af
Neelapu Nani <neelapunani2580@gmail.com>
2026-03-02 03:25:05 +0530

    Clearing the scuace page

--- app/sauces/saucepageone.jsx
+++ app/sauces/saucepageone.jsx
@@ -14,9 +14,8 @@ const saucesData = [
     {
         id: 2,
         title: "HONEY GLAZE BBQ",
-        descLine1: "Sweet and sticky blend of honey and smoke",
-        descLine2: "perfectly balanced for all your BBQ needs.",
-        descLine3: "Made with authentic ingredients",
+        descLine1: "Tomato base slow-simmered with smoked spices and finished with ",
+        descLine2: "pure honey.",
         bgUrl: "https://ehtazgziwtjqm5ww.public.blob.vercel-storage.com/sauce/Sauce2.webp", // Replace with real image later
         sauceUrl: "https://ehtazgziwtjqm5ww.public.blob.vercel-storage.com/sauce/sac%202.webp" // Replace with real image later
     }
