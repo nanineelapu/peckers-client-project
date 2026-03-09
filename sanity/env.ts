03c5b2c4d0021ea788026fbb11a0907b5cb8309b
Neelapu Nani <neelapunani2580@gmail.com>
2026-03-09 13:42:25 +0530

    De

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,8 +1,8 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-03-07'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2026-03-07"
 
 export const dataset =
-  process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+  process.env.NEXT_PUBLIC_SANITY_DATASET || "production"
 
 export const projectId =
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || ''
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "3gu4dx3n"
